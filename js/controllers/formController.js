ac031ed3e25409fe3c64170a53b9dfe3de4ebffd
vladlesun <vladlesunmma@gmail.com>
2023-07-12 10:22:44 +0300

    add form

--- js/controllers/formController.js
+++ js/controllers/formController.js
@@ -1,5 +1,23 @@
 import { validation } from "../modules/validation.js";
 
+const modal = document.querySelector(".modal");
+const body = document.querySelector("body");
+const allInputs = document.querySelectorAll("input"),
+  allTextarea = document.querySelectorAll("textarea");
+
+function getItem(allItems) {
+  for (let item of allItems) {
+    item.value = "";
+  }
+}
+
+modal.addEventListener("click", ({ target, currentTarget }) => {
+  if (target.closest(".modal.modal_open") || target === currentTarget) {
+    currentTarget.classList.remove("modal_open");
+    body.classList.remove("is-hidden");
+  }
+});
+
 export function formController() {
   const formSubmit = document.getElementById("add-form");
 
@@ -7,7 +25,11 @@ export function formController() {
     elem.preventDefault();
 
     if (validation(this) === true) {
-      alert("Форма проверена успешно!");
+      document.getElementById("header").scrollIntoView({ behavior: "smooth" });
+      modal.classList.add("modal_open");
+      body.classList.add("is-hidden");
+      getItem(allInputs);
+      getItem(allTextarea);
     }
   });
 }
